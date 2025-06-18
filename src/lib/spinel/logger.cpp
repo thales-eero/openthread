09d062859d9e58e6bcbc88c8039999e448944f62
Thales Nascimento <thales@eero.com>
2025-06-18 18:02:18 -0300

    Add logs(3)

--- src/lib/spinel/logger.cpp
+++ src/lib/spinel/logger.cpp
@@ -122,32 +122,6 @@ uint32_t Logger::Snprintf(char *aDest, uint32_t aSize, const char *aFormat, ...)
 
 void Logger::LogSpinelFrame(const uint8_t *aFrame, uint16_t aLength, bool aTx)
 {
-    {
-        static char dst[1000];
-        char *d = dst;
-        size_t sz = sizeof(dst);
-
-        const int cols = 16;
-        const int len = aLength;
-        for (int i = 0; i < len; i += cols) {
-            for (int j = 0; j < cols && i + j < len; j += 1) {
-                written = snprintf(d, sz, "%02x", aFrame[i + j]);
-                d += written;
-                sz -= written;
-                if (j < cols - 1) {
-                    written = snprintf(d, sz, " ");
-                    d += written;
-                    sz -= written;
-                }
-            }
-
-            written = snprintf(d, sz, "\r\n");
-            d += written;
-            sz -= written;
-        }
-        LogWarn("[spin %s spin]", dst);
-    }
-
     otError           error                                   = OT_ERROR_NONE;
     char              buf[OPENTHREAD_LIB_SPINEL_LOG_MAX_SIZE] = {0};
     spinel_ssize_t    unpacked;
@@ -773,6 +747,32 @@ void Logger::LogSpinelFrame(const uint8_t *aFrame, uint16_t aLength, bool aTx)
     }
 
 exit:
+    {
+        static char dst[1000];
+        char *d = dst;
+        size_t sz = sizeof(dst);
+
+        const int cols = 16;
+        const int lenx = aLength;
+        for (int i = 0; i < lenx; i += cols) {
+            for (int j = 0; j < cols && i + j < lenx; j += 1) {
+                int written = snprintf(d, sz, "%02x", aFrame[i + j]);
+                d += written;
+                sz -= written;
+                if (j < cols - 1) {
+                    written = snprintf(d, sz, " ");
+                    d += written;
+                    sz -= written;
+                }
+            }
+
+            int written = snprintf(d, sz, "\r\n");
+            d += written;
+            sz -= written;
+        }
+        start += Snprintf(start, static_cast<uint32_t>(end - start), " [dump %s ]", dst);
+    }
+
     OT_UNUSED_VARIABLE(start); // Avoid static analysis error
     if (error == OT_ERROR_NONE)
     {
